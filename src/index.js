27b2d463750f998ab586ae4113ded0eceda73087
Raj Prajapati <praj69577@gmail.com>
2025-04-29 12:17:51 +0530

    chcanges

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import express from "express";
 import doent from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
+
 doent.config();
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
+
 import authRoutes from "./routes/authRouter.js";
 import messageRoute from "./routes/messageRouter.js";
 import { dbConnect } from "./lib/dbConnect.js";
@@ -21,12 +21,6 @@ app.use(
 );
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoute);
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 server.listen(PORT, () => {
   console.log(`listing on PORT ${PORT}`);
