aa41955bc6b789952b3130222be15c0a888d7ba5
Raj Prajapati <praj69577@gmail.com>
2025-05-02 13:03:09 +0530

    url update

--- src/index.js
+++ src/index.js
@@ -13,9 +13,20 @@ import { app, server } from "./lib/socket.js";
 app.use(express.json({ limit: "10mb" })); // or even more if needed
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL,
+];
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
--- src/lib/socket.js
+++ src/lib/socket.js
@@ -4,11 +4,14 @@ import express from "express";
 
 const app = express();
 const server = http.createServer(app);
+const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL].filter(Boolean);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173",process.env.FRONTEND_URL],
+    origin: allowedOrigins,
+    credentials: true,
   },
 });
+
 export function getReceiverSoketId(userId){
   return userSoketMap[userId]
 }
